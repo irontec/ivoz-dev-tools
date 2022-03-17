15581646cb52e0c44021797fd11bb3a7948d1159
Roger Batista <rbatista@irontec.com>
2022-03-17 10:19:47 +0100

    Remove empty comments

--- EntityGeneratorBundle/Resources/skeleton/doctrine/RepositoryInterface.tpl.php
+++ EntityGeneratorBundle/Resources/skeleton/doctrine/RepositoryInterface.tpl.php
@@ -4,9 +4,6 @@ namespace <?= $namespace ?>;
 use Doctrine\Common\Collections\Selectable;
 use Doctrine\Persistence\ObjectRepository;
 
-/**
-*
-*/
 interface <?= $class_name  ?> extends ObjectRepository, Selectable
 {
 
