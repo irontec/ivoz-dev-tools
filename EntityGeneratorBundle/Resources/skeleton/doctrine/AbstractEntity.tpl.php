0d29a6a2777584961f7be547b285b7fa68ef4899
Mikel Madariaga <mikel@irontec.com>
2023-02-08 16:44:59 +0100

    updated ivoz-core

--- EntityGeneratorBundle/Resources/skeleton/doctrine/AbstractEntity.tpl.php
+++ EntityGeneratorBundle/Resources/skeleton/doctrine/AbstractEntity.tpl.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace <?= $namespace ?>;
 
 use Assert\Assertion;
-use Ivoz\Core\Application\DataTransferObjectInterface;
+use Ivoz\Core\Domain\\DataTransferObjectInterface;
 use Ivoz\Core\Domain\Model\ChangelogTrait;
 use Ivoz\Core\Domain\Model\EntityInterface;
-use Ivoz\Core\Application\ForeignKeyTransformerInterface;
+use Ivoz\Core\Domain\\ForeignKeyTransformerInterface;
 /*__class_use_statements*/
 /**
 * <?= $class_name ."\n" ?>
